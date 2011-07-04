b3762bc5b59d314f7ca6d7adc67cfdc03752904a
srz007 <srz007@gmail.com>
2011-07-04 12:22:32 +0800

    调整项目目录结构,便于使用maven构建

--- Redis4j/src/org/elk/redis4j/api/cache/ICacheTransaction.java
+++
@@ -1,9 +0,0 @@
-package org.elk.redis4j.api.cache;
-
-import org.elk.redis4j.api.ICache;
-import org.elk.redis4j.api.ITransaction;
-
-public interface ICacheTransaction extends ITransaction, ICache
-{
-
-}
--- Redis4j/src/org/elk/redis4j/api/database/IDatabaseTransaction.java
+++
@@ -1,9 +0,0 @@
-package org.elk.redis4j.api.database;
-
-import org.elk.redis4j.api.IPipelining;
-import org.elk.redis4j.api.ITransaction;
-
-public interface IDatabaseTransaction extends ITransaction, IPipelining
-{
-
-}
